7bf101b8ac85b62a067b80b8b168c30ac5e530d7
geneing <geneing@gmail.com>
2025-06-05 09:28:02 -0700

    Refactor kokoro_test.py to process multiple input text files and save generated audio outputs

--- scripts/kokoro_test.py
+++ scripts/kokoro_test.py
@@ -5,13 +5,25 @@ from kokoro import KPipeline
 # from IPython.display import display, Audio
 import soundfile as sf
 import torch
+import glob 
+import os
+
 
 pipeline = KPipeline(lang_code='a')
-text = '''
-[Kokoro](/kˈOkəɹO/) is an open-weight TTS model with 82 million parameters. Despite its lightweight architecture, it delivers comparable quality to larger models while being significantly faster and more cost-efficient. With Apache-licensed weights, [Kokoro](/kˈOkəɹO/) can be deployed anywhere from production environments to personal projects.
-'''
-generator = pipeline(text, voice='af_heart')
-for i, (gs, ps, audio) in enumerate(generator):
-    print(i, gs, ps)
-    # display(Audio(data=audio, rate=24000, autoplay=i==0))
-    sf.write(f'{i}.wav', audio, 24000)
+# text = '''
+# [Kokoro](/kˈOkəɹO/) is an open-weight TTS model with 82 million parameters. Despite its lightweight architecture, it delivers comparable quality to larger models while being significantly faster and more cost-efficient. With Apache-licensed weights, [Kokoro](/kˈOkəɹO/) can be deployed anywhere from production environments to personal projects.
+# '''
+
+fnames = glob.glob("../inputs/*.txt")
+for fname in fnames:
+    text = []
+    with open(fname, 'r') as f:
+        for line in f:
+            text.append(line.strip())
+
+        generator = pipeline(text, voice='af_heart')
+        full_audio = []
+        for i, (gs, ps, audio) in enumerate(generator):
+            full_audio.append(audio)
+        audio = torch.cat(full_audio, dim=-1)
+        sf.write(f'../outputs/kokoro/{os.path.basename(fname)}.wav', audio, 24000)
